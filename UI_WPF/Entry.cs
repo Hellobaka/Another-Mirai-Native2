707b09c32b5d17c8734c1eb6d209e52f5744aef0
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-10-21 22:35:31 +0800

    不许重复拉自己

---
+++ UI_WPF/Entry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Another_Mirai_Native.UI
+{
+    public class Entry
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                App.Main();
+            }
+            else
+            {
+                Another_Mirai_Native.Entry.Main(args);
+            }
+        }
+    }
+}
