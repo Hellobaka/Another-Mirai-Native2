a73619fd24b9632dd5c3028810aa51755500c955
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-10-21 22:39:17 +0800

    填充页面

---
+++ UI_WPF/Converters/TimestampConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Another_Mirai_Native.UI.Converters
+{
+    internal class TimestampConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Helper.TimeStamp2DateTime((long)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UI_WPF/Pages/LogPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Another_Mirai_Native.DB;
+using Another_Mirai_Native.Model;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Another_Mirai_Native.UI.Pages
+{
+    /// <summary>
+    /// LogPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class LogPage : Page
+    {
+        public LogPage()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public ObservableCollection<LogModel> LogCollections { get; set; } = new();
+
+        private void LogPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            LogHelper.LogAdded -= LogHelper_LogAdded;
+            LogHelper.LogAdded += LogHelper_LogAdded;
+            LogHelper.LogStatusUpdated -= LogHelper_LogStatusUpdated;
+            LogHelper.LogStatusUpdated += LogHelper_LogStatusUpdated;
+        }
+
+        private void LogHelper_LogStatusUpdated(int logId, string status)
+        {
+            var log = LogCollections.FirstOrDefault(x => x.id == logId);
+            if (log != null)
+            {
+                log.status = status;
+            }
+        }
+
+        private void LogHelper_LogAdded(int logId, LogModel log)
+        {
+            Dispatcher.Invoke(() => LogCollections.Add(log));
+        }
+    }
+}
