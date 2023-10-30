b934c3f34144c4ae299e2a86327ffdc98570e970
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-10-30 20:59:30 +0800

    缓存

---
+++ Another-Mirai-Native/RequestCache.cs
@@ -0,0 +1,11 @@
+﻿namespace Another_Mirai_Native
+{
+    public static class RequestCache
+    {
+        public static Dictionary<long, (long, string)> FriendRequest { get; set; } = new();
+
+        public static Dictionary<long, (long, string, long, string)> GroupRequest { get; set; } = new();
+
+        public static Dictionary<long, string> Message { get; set; } = new();
+    }
+}
