e9925e59e796edbac6f6cc839072c5e9660f7f97
Mr.喜 <863450594@qq.com>
2024-05-07 14:58:42 +0800

    升级框架为.net8 清理i18n文件夹工具 自动构建脚本

--- copy.bat
+++ copy.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 
+echo Net48...
 set ROOT=.
 set TARGET=%ROOT%\UI_WPF\bin\x86\Debug\net48\protocols
 set TARGET_Console=%ROOT%\Another-Mirai-Native\bin\x86\Debug\net48\protocols
@@ -10,6 +11,11 @@ set TARGET2=%ROOT%\UI_WPF\bin\x86\Debug\net48
 if not exist "%TARGET%" (
     mkdir "%TARGET%"
 )
+if not exist "%TARGET_Console%" (
+    mkdir "%TARGET_Console%"
+)
+
+echo Copy Net48 Protocol...
 
 :: 复制文件
 for %%F in (
@@ -26,7 +32,143 @@ for %%F in (
     )
 )
 
-:: 复制文件到第二个目标文件夹
+echo Copy Net48 Loader...
+
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net48\loaders
+if not exist "%DIR%" (
+    mkdir "%DIR%"
+)
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net48\loaders\Net8
+if not exist "%DIR%" (
+    mkdir "%DIR%"
+)
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net48\loaders\NetFramework48
+if not exist "%DIR%" (
+    mkdir "%DIR%"
+)
+
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net48\loaders\NetFramework48
+set FILE=%ROOT%\Another-Mirai-Native\bin\x86\Debug\net48\Another-Mirai-Native.exe
+if exist "%FILE%" (
+    copy /Y "%FILE%" "%DIR%"
+) else (
+    echo Warning: File "%FILE%" not found.
+)
+set FILE=%ROOT%\CQP\bin\x86\Debug\CQP.dll
+if exist "%FILE%" (
+    copy /Y "%FILE%" "%DIR%"
+) else (
+    echo Warning: File "%FILE%" not found.
+)
+
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net48\loaders\Net8
+for %%F in (
+    "%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\Another-Mirai-Native.deps.json"
+    "%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\Another-Mirai-Native.dll"
+    "%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\Another-Mirai-Native.exe"
+    "%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\Another-Mirai-Native.runtimeconfig.json"
+) do (
+    if exist "%%F" (
+        copy /Y "%%F" "%DIR%"
+    ) else (
+        echo Warning: File "%%F" not found.
+    )
+)
+
+set FILE="CleanI18nFolders.exe"
+set DIR="%ROOT%\UI_WPF\bin\x86\Debug\net48"
+copy "%FILE%" "%DIR%"
+%DIR%\%FILE%
+echo Delete CleanI18nFolders.exe
+del %DIR%\%FILE%
+
+set FILE=%ROOT%\CQP\bin\x86\Debug\CQP.dll
+if exist "%FILE%" (
+    copy /Y "%FILE%" "%TARGET2%"
+) else (
+    echo Warning: File "%FILE%" not found.
+)
+echo net8...
+
+set TARGET=%ROOT%\UI_WPF\bin\x86\Debug\net8.0-windows\protocols
+set TARGET_Console=%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\protocols
+set TARGET2=%ROOT%\UI_WPF\bin\x86\Debug\net8.0-windows
+
+:: 创建目标文件夹
+if not exist "%TARGET%" (
+    mkdir "%TARGET%"
+)
+if not exist "%TARGET_Console%" (
+    mkdir "%TARGET_Console%"
+)
+
+echo Copy Net8 Protocol...
+
+:: 复制文件
+for %%F in (
+    "%ROOT%\Protocol_OneBot\bin\x86\Debug\net8\Protocol_OneBotv11.dll"
+    "%ROOT%\Protocol_MiraiAPIHttp\bin\x86\Debug\net8\MiraiAPIHttp.dll"
+    "%ROOT%\Protocol_NoConnection\bin\x86\Debug\net8.0-windows\Protocol_NoConnection.dll"
+    "%ROOT%\Protocol_Satori_v1\bin\x86\Debug\net8\Protocol_Satori_v1.dll"
+) do (
+    if exist "%%F" (
+        copy /Y "%%F" "%TARGET%"
+        copy /Y "%%F" "%TARGET_Console%"
+    ) else (
+        echo Warning: File "%%F" not found.
+    )
+)
+
+echo Copy Net8 Loader...
+
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net8.0-windows\loaders
+if not exist "%DIR%" (
+    mkdir "%DIR%"
+)
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net8.0-windows\loaders\Net8
+if not exist "%DIR%" (
+    mkdir "%DIR%"
+)
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net8.0-windows\loaders\NetFramework48
+if not exist "%DIR%" (
+    mkdir "%DIR%"
+)
+
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net8.0-windows\loaders\NetFramework48
+set FILE=%ROOT%\Another-Mirai-Native\bin\x86\Debug\net48\Another-Mirai-Native.exe
+if exist "%FILE%" (
+    copy /Y "%FILE%" "%DIR%"
+) else (
+    echo Warning: File "%FILE%" not found.
+)
+set FILE=%ROOT%\CQP\bin\x86\Debug\CQP.dll
+if exist "%FILE%" (
+    copy /Y "%FILE%" "%DIR%"
+) else (
+    echo Warning: File "%FILE%" not found.
+)
+
+set DIR=%ROOT%\UI_WPF\bin\x86\Debug\net8.0-windows\loaders\Net8
+for %%F in (
+    "%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\Another-Mirai-Native.deps.json"
+    "%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\Another-Mirai-Native.dll"
+    "%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\Another-Mirai-Native.exe"
+    "%ROOT%\Another-Mirai-Native\bin\x86\Debug\net8.0-windows\Another-Mirai-Native.runtimeconfig.json"
+) do (
+    if exist "%%F" (
+        copy /Y "%%F" "%DIR%"
+    ) else (
+        echo Warning: File "%%F" not found.
+    )
+)
+
+set FILE="CleanI18nFolders.exe"
+set DIR="%ROOT%\UI_WPF\bin\x86\Debug\net8.0-windows"
+copy "%FILE%" "%DIR%"
+%DIR%\%FILE%
+echo Delete CleanI18nFolders.exe
+del %DIR%\%FILE%
+
 set FILE=%ROOT%\CQP\bin\x86\Debug\CQP.dll
 if exist "%FILE%" (
     copy /Y "%FILE%" "%TARGET2%"
