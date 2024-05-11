7da1b910c2f33c28667f32611f2df52e31709b1a
Mr.喜 <863450594@qq.com>
2024-05-11 16:40:02 +0800

    添加WebUI框架

---
+++ UI_Blazor/Program.cs
@@ -0,0 +1,35 @@
+using MudBlazor.Services;
+using UI_Blazor.Components;
+
+namespace UI_Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddMudServices();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
