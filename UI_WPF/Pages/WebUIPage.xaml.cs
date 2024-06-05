723015dbfe59055b62c2b57538dccd74787dfbb2
Mr.喜 <863450594@qq.com>
2024-06-05 10:23:03 +0800

    添加WebUI页面 移动部分文件

---
+++ UI_WPF/Pages/WebUIPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Another_Mirai_Native.UI.Pages
+{
+    /// <summary>
+    /// WebUIPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class WebUIPage : Page
+    {
+        public WebUIPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void WebUIStartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProcessStartStatus.Fill == Brushes.Green)
+            {
+                DialogHelper.ShowSimpleDialog("启动 WebUI", "服务无法重复启动");
+                return;
+            }
+            if (await DialogHelper.ShowConfirmDialog("启动 WebUI", "确定要启动 WebUI 吗？") is false)
+            {
+                return;
+            }
+#if NET5_0_OR_GREATER
+            ObservableTextWriter writer = new();
+            writer.OnWrite += Writer_OnWrite;
+            ObservableTextWriter errorWriter = new();
+            errorWriter.OnWrite += ErrorWriter_OnWrite;
+            Console.SetOut(writer);
+            Console.SetError(errorWriter);
+            BlazorUI.Program.OnBlazorServiceStarted += Program_OnBlazorServiceStarted;
+            BlazorUI.Program.OnBlazorServiceStoped += Program_OnBlazorServiceStoped;
+
+            Task.Run(() => BlazorUI.Program.Main([]));
+#else
+            DialogHelper.ShowSimpleDialog("启动 WebUI", "启动 WebUI 需要 .net8 以上版本");
+#endif
+        }
+
+        private void Program_OnBlazorServiceStoped()
+        {
+            Dispatcher.InvokeAsync(() =>
+            {
+                ProcessStartStatus.Fill = Brushes.Red;
+                ProcessStartText.Text = "服务已退出";
+            });
+        }
+
+        private void Program_OnBlazorServiceStarted()
+        {
+            Dispatcher.InvokeAsync(() =>
+            {
+                ProcessStartStatus.Fill = Brushes.Green;
+                ProcessStartText.Text = "服务已启动";
+            });
+        }
+
+        private async void ErrorWriter_OnWrite(string msg)
+        {
+            await Dispatcher.InvokeAsync(() =>
+            {
+                Terminal_Error.AppendText(msg);
+                ScrollContainer_Error.ScrollToEnd();
+            });
+        }
+
+        private async void Writer_OnWrite(string msg)
+        {
+            await Dispatcher.InvokeAsync(() =>
+            {
+                Terminal_Output.AppendText(msg);
+                ScrollContainer_Output.ScrollToEnd();
+            });
+        }
+
+        private async void WebUIStopButton_Click(object sender, RoutedEventArgs e)
+        {
+#if NET5_0_OR_GREATER
+            if (await DialogHelper.ShowConfirmDialog("终止 WebUI", "确定要终止 WebUI 吗？") is false)
+            {
+                return;
+            }
+            await BlazorUI.Program.BlazorHost.StopAsync();
+#endif
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void TerminalOutputClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Terminal_Output.Text = "";
+        }
+
+        private void TerminalErrorClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Terminal_Error.Text = "";
+        }
+
+        private class ObservableTextWriter : TextWriter
+        {
+            public override Encoding Encoding => Encoding.Default;
+
+            public event Action<string> OnWrite;
+
+            public override void Write(char value)
+            {
+                OnWrite?.Invoke(value.ToString());
+            }
+
+            public override void Write(string value)
+            {
+                OnWrite?.Invoke(value);
+            }
+
+            public override void WriteLine(string value)
+            {
+                OnWrite?.Invoke(value + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ UI_WPF/WinNative/JobObject.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Another_Mirai_Native.UI.WinNative
+{
+    public class JobObject
+    {
+        private enum JobObjectInfoType
+        {
+            AssociateCompletionPortInformation = 7,
+
+            BasicLimitInformation = 2,
+
+            BasicUIRestrictions = 4,
+
+            EndOfJobTimeInformation = 6,
+
+            ExtendedLimitInformation = 9,
+
+            SecurityLimitInformation = 5,
+
+            GroupInformation = 11
+        }
+
+        public static void AssignProcessToKillOnCloseJob(Process process)
+        {
+            var job = CreateJobObject(IntPtr.Zero, null);
+
+            var info = new JOBOBJECT_BASIC_LIMIT_INFORMATION { LimitFlags = 0x2000 };
+
+            var extendedInfo = new JOBOBJECT_EXTENDED_LIMIT_INFORMATION { BasicLimitInformation = info };
+
+            int length = Marshal.SizeOf(typeof(JOBOBJECT_EXTENDED_LIMIT_INFORMATION));
+            IntPtr extendedInfoPtr = Marshal.AllocHGlobal(length);
+            Marshal.StructureToPtr(extendedInfo, extendedInfoPtr, false);
+
+            if (!SetInformationJobObject(job, JobObjectInfoType.ExtendedLimitInformation, extendedInfoPtr, (uint)length))
+            {
+                throw new System.ComponentModel.Win32Exception();
+            }
+
+            AssignProcessToJobObject(job, process.Handle);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool AssignProcessToJobObject(IntPtr job, IntPtr process);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateJobObject(IntPtr a, string lpName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetInformationJobObject(IntPtr hJob, JobObjectInfoType infoType, IntPtr lpJobObjectInfo, uint cbJobObjectInfoLength);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct IO_COUNTERS
+        {
+            public ulong ReadOperationCount;
+
+            public ulong WriteOperationCount;
+
+            public ulong OtherOperationCount;
+
+            public ulong ReadTransferCount;
+
+            public ulong WriteTransferCount;
+
+            public ulong OtherTransferCount;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct JOBOBJECT_BASIC_LIMIT_INFORMATION
+        {
+            public long PerProcessUserTimeLimit;
+
+            public long PerJobUserTimeLimit;
+
+            public short LimitFlags;
+
+            public IntPtr MinimumWorkingSetSize;
+
+            public IntPtr MaximumWorkingSetSize;
+
+            public short ActiveProcessLimit;
+
+            public long Affinity;
+
+            public short PriorityClass;
+
+            public short SchedulingClass;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct JOBOBJECT_EXTENDED_LIMIT_INFORMATION
+        {
+            public JOBOBJECT_BASIC_LIMIT_INFORMATION BasicLimitInformation;
+
+            public IO_COUNTERS IoInfo;
+
+            public IntPtr ProcessMemoryLimit;
+
+            public IntPtr JobMemoryLimit;
+
+            public IntPtr PeakProcessMemoryUsed;
+
+            public IntPtr PeakJobMemoryUsed;
+        }
+    }
+}
