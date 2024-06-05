dfd8a326d2b664fe0a9e620e6a587853b73d0116
Mr.喜 <863450594@qq.com>
2024-06-05 10:23:26 +0800

    外抛Blazor启动状态 删除部分逻辑

--- UI_Blazor/Program.cs
+++ UI_Blazor/Program.cs
@@ -3,11 +3,17 @@ using MudBlazor.Services;
 using Another_Mirai_Native.BlazorUI.Components;
 using Another_Mirai_Native.BlazorUI.Models;
 using System.Text;
+using Microsoft.Extensions.Hosting;
 
 namespace Another_Mirai_Native.BlazorUI
 {
     public class Program
     {
+        public static event Action OnBlazorServiceStarted;
+        public static event Action OnBlazorServiceStoped;
+
+        public static IHost BlazorHost { get; private set; }
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +30,18 @@ namespace Another_Mirai_Native.BlazorUI
             builder.Services.AddScoped<AuthenticationStateProvider>(p => p.GetRequiredService<AuthService>());
 
             var app = builder.Build();
+            BlazorHost = app;
+            var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+
+            lifetime.ApplicationStarted.Register(() =>
+            {
+                OnBlazorServiceStarted?.Invoke();
+            });
+
+            lifetime.ApplicationStopped.Register(() =>
+            {
+                OnBlazorServiceStoped?.Invoke();
+            });
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -37,46 +55,7 @@ namespace Another_Mirai_Native.BlazorUI
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
 
-            Console.SetOut(new ObservableTextWriter(Console.Out));
-
             app.Run();
-        }
-
-        public class ObservableTextWriter : TextWriter
-        {
-            private static TextWriter _consoleWriter;
-
-            public static event Action<string> OnConsoleOutput;
-
-            public ObservableTextWriter(TextWriter stream)
-            {
-                _consoleWriter = stream;
-            }
-
-            public override Encoding Encoding => _consoleWriter.Encoding;
-
-            public override void Write(char value)
-            {
-                _consoleWriter.Write(value);
-                OnConsoleOutput?.Invoke(value.ToString());
-            }
-
-            public override void Write(string value)
-            {
-                _consoleWriter.Write(value);
-                OnConsoleOutput?.Invoke(value);
-            }
-
-            public override void WriteLine(string value)
-            {
-                _consoleWriter.WriteLine(value);
-                OnConsoleOutput?.Invoke(value + Environment.NewLine);
-            }
-
-            public override void Flush()
-            {
-                _consoleWriter.Flush();
-            }
         }
     }
 }
