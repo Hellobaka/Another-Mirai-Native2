5418cc045695e64cbfce95206bd79edf8f49b4a8
Mr.喜 <863450594@qq.com>
2024-06-07 12:43:12 +0800

    实现插件管理页面

---
+++ UI_Blazor/Models/CQPluginProxyWrapper.cs
@@ -0,0 +1,20 @@
+﻿using Another_Mirai_Native.Native;
+
+namespace Another_Mirai_Native.BlazorUI.Models
+{
+    public class CQPluginProxyWrapper
+    {
+        public CQPluginProxy TargetPlugin { get; set; }
+
+        public bool Selected { get; set; }
+
+        public CQPluginProxyWrapper(CQPluginProxy proxy)
+        {
+            TargetPlugin = proxy;
+            if (string.IsNullOrEmpty(TargetPlugin.PluginId))
+            {
+                TargetPlugin.AppInfo.AppId = "启用插件以查看 AppId";
+            }
+        }
+    }
+}
