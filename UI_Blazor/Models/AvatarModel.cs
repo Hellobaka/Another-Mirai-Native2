01fd0b8625f53e0173806c9e3d4b8ab988174ac5
Mr.喜 <863450594@qq.com>
2024-06-07 16:40:57 +0800

    实现气泡组件

---
+++ UI_Blazor/Models/AvatarModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Another_Mirai_Native.BlazorUI.Models
+{
+    public class AvatarModel
+    {
+        public AvatarTypes AvatarType { get; set; } = AvatarTypes.Fallback;
+
+        public string FallbackName { get; set; } = "";
+
+        public long Id { get; set; }
+
+        public bool IsRound { get; set; }
+
+        public string BackgroundColor { get; set; }
+    }
+}
---
+++ UI_Blazor/Models/ChatItemModel.cs
@@ -0,0 +1,75 @@
+﻿namespace Another_Mirai_Native.BlazorUI.Models
+{
+    public enum DetailItemType
+    {
+        Notice,
+
+        Receive,
+
+        Send
+    }
+
+    public enum AvatarTypes
+    {
+        QQGroup,
+
+        QQPrivate,
+
+        Fallback
+    }
+
+    public class ChatItemModel
+    {
+        /// <summary>
+        /// 消息位置
+        /// </summary>
+        public DetailItemType DetailItemType { get; set; }
+
+        /// <summary>
+        /// 显示的名称
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// 消息块的GUID
+        /// </summary>
+        public string GUID { get; set; }
+
+        /// <summary>
+        /// 消息所属的QQ
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 未转换的消息内容
+        /// </summary>
+        public string Message { get; set; } = "";
+
+        /// <summary>
+        /// 消息ID
+        /// </summary>
+        public int MsgId { get; set; }
+
+        /// <summary>
+        /// 消息所属的组ID 群号或QQ
+        /// </summary>
+        public long ParentId { get; set; }
+
+        /// <summary>
+        /// 消息来源
+        /// </summary>
+        public AvatarTypes ParentType { get; set; } = AvatarTypes.Fallback;
+
+        /// <summary>
+        /// 是否已撤回
+        /// </summary>
+        public bool Recalled { get; set; }
+
+        /// <summary>
+        /// 显示的时间
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        public AvatarModel Avatar { get; set; } = new AvatarModel();
+    }
+}
