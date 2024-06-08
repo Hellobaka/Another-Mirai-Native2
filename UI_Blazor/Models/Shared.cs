c4489a0a0502a39044a65758ccb99aa20e152dcc
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2024-06-08 22:30:25 +0800

    实现测试页面逻辑

--- UI_Blazor/Models/Shared.cs
+++ UI_Blazor/Models/Shared.cs
@@ -5,5 +5,7 @@
         public bool IsDarkMode { get; set; } = true;
 
         public bool AutoScroll { get; set; } = true;
+
+        public List<ChatItemModel> TestMessages { get; set; } = [];
     }
 }
