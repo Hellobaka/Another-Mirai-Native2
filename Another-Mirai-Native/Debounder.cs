57b990e5c5d128be189e177d1578b1d8ec991a18
Mr.喜 <863450594@qq.com>
2024-06-14 11:16:20 +0800

    防抖帮助类

---
+++ Another-Mirai-Native/Debounder.cs
@@ -0,0 +1,28 @@
+﻿namespace Another_Mirai_Native
+{
+    public class Debouncer
+    {
+        private CancellationTokenSource cancellationTokenSource = new();
+
+        /// <summary>
+        /// 防抖
+        /// </summary>
+        /// <param name="callback">要执行的方法</param>
+        /// <param name="waitTime">等待时间（毫秒）</param>
+        public void Debounce(Action callback, int waitTime)
+        {
+            // 取消之前的延时调用
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource = new CancellationTokenSource();
+
+            Task.Delay(waitTime, cancellationTokenSource.Token)
+                .ContinueWith(task =>
+                {
+                    if (!task.IsCanceled)
+                    {
+                        callback();
+                    }
+                });
+        }
+    }
+}
