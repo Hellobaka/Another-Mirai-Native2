2be1a6d108fa6a2414501a319af3af647a3aedef
Mr.喜 <863450594@qq.com>
2024-06-17 16:43:11 +0800

    实现显示插件资源占用状态

---
+++ UI_Blazor/Models/DashBoardProcessItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Another_Mirai_Native.BlazorUI.Models
+{
+    public class DashBoardProcessItem
+    {
+        public int PID { get; set; }
+
+        public string PluginName { get; set; } = "";
+
+        public bool Running { get; set; }
+
+        public double CPUUsage { get; set; }
+       
+        public double MemoryUsage { get; set; }
+    }
+}
