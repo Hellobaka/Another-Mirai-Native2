a144f54220c3483d0f19e259a7a9b9933680cb00
Mr.喜 <863450594@qq.com>
2025-01-13 09:50:01 +0800

    更新二维码兼容输出模式

--- Another-Mirai-Native/UI/QRCodeHelper.cs
+++ Another-Mirai-Native/UI/QRCodeHelper.cs
@@ -8,45 +8,69 @@ namespace Another_Mirai_Native
     {
         // This part of the code is from "https://github.com/eric2788/Lagrange.Core/blob/fd20a5aec81cacd56d60f3130cf057461300fd3f/Lagrange.OneBot/Utility/QrCodeHelper.cs#L30C52-L30C52"
         // Thanks to "https://github.com/eric2788"
+        // https://github.com/LagrangeDev/Lagrange.Core/issues/732
         public static void Output(string text, bool compatibilityMode)
         {
 #if NET5_0_OR_GREATER
             var segments = QrSegment.MakeSegments(text);
             var qrCode = QrCode.EncodeSegments(segments, QrCode.Ecc.Low);
 
-            var (bottomHalfBlock, topHalfBlock, emptyBlock, fullBlock) = compatibilityMode ? (".", "^", " ", "@") : ("▄", "▀", " ", "█");
-
-            for (var y = 0; y < qrCode.Size + 2; y += 2)
-            {
-                for (var x = 0; x < qrCode.Size + 2; ++x)
-                {
-                    var foregroundBlack = qrCode.GetModule(x - 1, y - 1);
-                    var backgroundBlack = qrCode.GetModule(x - 1, y) || y > qrCode.Size;
-
-                    if (foregroundBlack && !backgroundBlack)
-                    {
-                        Console.Write(bottomHalfBlock);
-                    }
-                    else if (!foregroundBlack && backgroundBlack)
-                    {
-                        Console.Write(topHalfBlock);
-                    }
-                    else if (foregroundBlack && backgroundBlack)
-                    {
-                        Console.Write(emptyBlock);
-
-                    }
-                    else if (!foregroundBlack && !backgroundBlack)
-                    {
-                        Console.Write(fullBlock);
-                    }
-                }
-                Console.Write("\n");
-            }
-
             if (compatibilityMode)
             {
-                Console.WriteLine("Please scan this QR code from a distance with your smart phone.\nScanning may fail if you are too close.");
+                for (var y = 0; y < qrCode.Size; y++)
+                {
+                    for (var x = 0; x < qrCode.Size; x++)
+                    {
+                        var color = qrCode.GetModule(x, y);
+                        if (color)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write("  ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.Write("  ");
+                            Console.ResetColor();
+                        }
+                    }
+                    Console.Write("\n");
+                }
+            }
+            else
+            {
+                var (bottomHalfBlock, topHalfBlock, emptyBlock, fullBlock) = ("▄", "▀", " ", "█");
+
+                for (var y = 0; y < qrCode.Size + 2; y += 2)
+                {
+                    for (var x = 0; x < qrCode.Size + 2; ++x)
+                    {
+                        var foregroundBlack = qrCode.GetModule(x - 1, y - 1);
+                        var backgroundBlack = qrCode.GetModule(x - 1, y) || y > qrCode.Size;
+
+                        if (foregroundBlack && !backgroundBlack)
+                        {
+                            Console.Write(bottomHalfBlock);
+                        }
+                        else if (!foregroundBlack && backgroundBlack)
+                        {
+                            Console.Write(topHalfBlock);
+                        }
+                        else if (foregroundBlack && backgroundBlack)
+                        {
+                            Console.Write(emptyBlock);
+
+                        }
+                        else if (!foregroundBlack && !backgroundBlack)
+                        {
+                            Console.Write(fullBlock);
+                        }
+                    }
+                    Console.Write("\n");
+                }
             }
 #endif
         }
