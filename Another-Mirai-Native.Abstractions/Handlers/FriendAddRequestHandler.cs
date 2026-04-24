010d8bbb2fe03a3303f8aa91282ff910d50e712a
Mr.喜 <863450594@qq.com>
2026-04-24 17:24:22 +0800

    Typo

--- Another-Mirai-Native.Abstractions/Handlers/FriendAddRequestHandler.cs
+++ Another-Mirai-Native.Abstractions/Handlers/FriendAddRequestHandler.cs
@@ -12,7 +12,7 @@ namespace Another_Mirai_Native.Abstractions.Handlers
     public interface IFriendAddRequestHandler
     {
         /// <summary>
-        /// 在收到好友添加请求时触发。可以通过重写此方法来处理好友添加请求，例如自动接受、拒绝或忽略请求。不重写时默认行为是忽略请求，即不做任何处理。处理请求请调用参数的 <see href="FriendAddedContext.SetRequestResult"/> 方法
+        /// 在收到好友添加请求时触发。可以通过重写此方法来处理好友添加请求，例如自动接受、拒绝或忽略请求。不重写时默认行为是忽略请求，即不做任何处理。处理请求请调用参数的 <see cref="FriendAddRequestContext.SetRequestResult"/> 方法
         /// </summary>
         /// <param name="e">包含好友添加请求的详细信息。</param>
         /// <param name="ct">可用于发出退出操作取消信号的取消令牌。</param>
