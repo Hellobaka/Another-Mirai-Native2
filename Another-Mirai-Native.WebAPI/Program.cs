405aa5fc46e44130755c88789b381370d17dfc36
Mr.喜 <863450594@qq.com>
2026-05-21 17:08:57 +0800

    WebAPI项目创建

---
+++ Another-Mirai-Native.WebAPI/Program.cs
@@ -0,0 +1,32 @@
+
+namespace Another_Mirai_Native.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
