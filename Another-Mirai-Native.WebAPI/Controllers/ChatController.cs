f9e17b89c1ae324bc5ddaf465ca7d525f0e0c11c
Mr.喜 <863450594@qq.com>
2026-05-26 16:36:52 +0800

    完善ChatController的OpenAPI注释

--- Another-Mirai-Native.WebAPI/Controllers/ChatController.cs
+++ Another-Mirai-Native.WebAPI/Controllers/ChatController.cs
@@ -5,9 +5,9 @@ using Another_Mirai_Native.DB;
 using Another_Mirai_Native.Model;
 using Another_Mirai_Native.Native;
 using Another_Mirai_Native.WebAPI.Models;
-using Azure.Core;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 using System.Diagnostics;
 
 namespace Another_Mirai_Native.WebAPI.Controllers
@@ -18,6 +18,10 @@ namespace Another_Mirai_Native.WebAPI.Controllers
     public class ChatController : ControllerBase
     {
         [HttpGet("categories")]
+        [EndpointSummary("获取聊天会话列表")]
+        [EndpointDescription("获取所有聊天会话分类，按最后一条消息降序排列。message 字段为已解析的消息链，time 已转为 DateTime")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetChatCategories()
         {
             if (WebUIConfig.Instance.EnableChat is false)
@@ -29,7 +33,15 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpGet("history")]
-        public async Task<IActionResult> GetChatHistory(ChatHistoryType chatHistoryType, long parentId, int pageIndex = 1, int pageSize = 50)
+        [EndpointSummary("分页查询聊天记录")]
+        [EndpointDescription("按会话分页获取聊天记录")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetChatHistory(
+            [Description("聊天类型：0=群聊，1=私聊")] ChatHistoryType chatHistoryType,
+            [Description("群号或好友 QQ")] long parentId,
+            [Description("页码，从 1 开始")] int pageIndex = 1,
+            [Description("每页条数")] int pageSize = 50)
         {
             if (WebUIConfig.Instance.EnableChat is false)
             {
@@ -41,7 +53,14 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpGet("message")]
-        public async Task<IActionResult> GetChatMessage(ChatHistoryType chatHistoryType, long parentId, int messageId)
+        [EndpointSummary("查询单条消息")]
+        [EndpointDescription("按消息 ID 查询单条聊天记录")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetChatMessage(
+            [Description("聊天类型：0=群聊，1=私聊")] ChatHistoryType chatHistoryType,
+            [Description("群号或好友 QQ")] long parentId,
+            [Description("消息 ID")] int messageId)
         {
             if (WebUIConfig.Instance.EnableChat is false)
             {
@@ -57,28 +76,45 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpGet("friend-nick")]
-        public async Task<IActionResult> GetFriendNick(long qq)
+        [EndpointSummary("获取好友昵称")]
+        [EndpointDescription("根据 QQ 号查询好友昵称")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetFriendNick([Description("好友 QQ 号")] long qq)
         {
             string nick = await ChatHistoryHelper.GetFriendNick(qq);
             return Ok(ApiResponse.Ok(new { nick }));
         }
 
         [HttpGet("group-name")]
-        public async Task<IActionResult> GetGroupName(long groupId)
+        [EndpointSummary("获取群名称")]
+        [EndpointDescription("根据群号查询群名称")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetGroupName([Description("群号")] long groupId)
         {
             string groupName = await ChatHistoryHelper.GetGroupName(groupId);
             return Ok(ApiResponse.Ok(new { groupName }));
         }
 
         [HttpGet("group-member-card")]
-        public async Task<IActionResult> GetGroupMemberCard(long groupId, long qq)
+        [EndpointSummary("获取群成员名片")]
+        [EndpointDescription("根据群号和 QQ 号查询群成员名片")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetGroupMemberCard(
+            [Description("群号")] long groupId,
+            [Description("群成员 QQ 号")] long qq)
         {
             string card = await ChatHistoryHelper.GetGroupMemberNick(groupId, qq);
             return Ok(ApiResponse.Ok(new { card }));
         }
 
         [HttpPost("clear-unread")]
-        public async Task<IActionResult> ClearUnread(ChatHistoryType chatHistoryType, long parentId)
+        [EndpointSummary("清除未读计数")]
+        [EndpointDescription("将指定会话的未读消息计数清零")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> ClearUnread(
+            [Description("聊天类型：0=群聊，1=私聊")] ChatHistoryType chatHistoryType,
+            [Description("群号或好友 QQ")] long parentId)
         {
             if (WebUIConfig.Instance.EnableChat is false)
             {
@@ -89,7 +125,13 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpPost("send")]
-        public async Task<IActionResult> SendMessage([FromBody] ChatMessageSendRequest request)
+        [EndpointSummary("发送消息")]
+        [EndpointDescription("发送群聊或私聊消息，content 使用原始 CQ 码格式。成功返回消息 ID")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> SendMessage(
+            [Description("消息发送请求体")] [FromBody] ChatMessageSendRequest request)
         {
             if (WebUIConfig.Instance.EnableChat is false)
             {
@@ -140,7 +182,12 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpGet("message-chain")]
-        public IActionResult ConvertToMessageChain(string message)
+        [EndpointSummary("CQ 码转消息链")]
+        [EndpointDescription("将原始 CQ 码字符串解析为结构化的 MessageItemBase 数组，用于发送前预览")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public IActionResult ConvertToMessageChain(
+            [Description("原始 CQ 码字符串")] string message)
         {
             if (WebUIConfig.Instance.EnableChat is false)
             {
@@ -150,7 +197,15 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpPost("recall")]
-        public async Task<IActionResult> RecallMessage(ChatHistoryType chatHistoryType, long parentId, int messageId)
+        [EndpointSummary("撤回消息")]
+        [EndpointDescription("撤回指定会话中的指定消息，成功后将消息标记为已撤回并触发插件事件")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> RecallMessage(
+            [Description("聊天类型：0=群聊，1=私聊")] ChatHistoryType chatHistoryType,
+            [Description("群号或好友 QQ")] long parentId,
+            [Description("消息 ID")] int messageId)
         {
             if (WebUIConfig.Instance.EnableChat is false)
             {
@@ -189,6 +244,10 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpGet("collected")]
+        [EndpointSummary("获取收藏图片列表")]
+        [EndpointDescription("获取已收藏图片的文件名列表")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetCollectedPictures()
         {
             if (WebUIConfig.Instance.EnableChat is false)
@@ -209,7 +268,13 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpPost("collect")]
-        public async Task<IActionResult> CollectPicture(string file)
+        [EndpointSummary("收藏图片")]
+        [EndpointDescription("将已缓存的图片按哈希复制到收藏目录")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CollectPicture(
+            [Description("图片缓存哈希")] string file)
         {
             if (WebUIConfig.Instance.EnableChat is false)
             {
@@ -237,7 +302,14 @@ namespace Another_Mirai_Native.WebAPI.Controllers
         }
 
         [HttpPost("upload-picture")]
-        public async Task<IActionResult> UploadPicture(IFormFile file)
+        [EndpointSummary("上传图片")]
+        [EndpointDescription("上传图片文件到 data/image 目录，返回构造好的 Image 消息片段。仅允许 PNG/JPG/JPEG/GIF 格式")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UploadPicture(
+            [Description("图片文件（multipart/form-data）")] IFormFile file)
         {
             if (WebUIConfig.Instance.EnableChat is false)
             {
